14921c7b7a6a569f2643058b2b6be0b559dc9348
Wenjun Liao <wenjun.liao@zispire.com>
2023-10-28 10:06:51 +0200

    Update website

--- app.py
+++ app.py
@@ -1,18 +1,64 @@
-from flask import Flask, render_template, request
-import random
+from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+from werkzeug.utils import secure_filename
+import PyPDF2
+from docx import Document
+# Replace these placeholders with your actual values
+TENANT = "platformzispire"
+POLICY = "B2C_1_susi"
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'pdf', 'docx', 'txt'}
+app.secret_key = "supersecretkey"
 
-# Sample names for our generator
-first_names = ["John", "Jane", "Alex", "Emily", "Chris", "Katie"]
-last_names = ["Smith", "Doe", "Johnson", "Brown", "Davis", "Miller"]
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    name = ""
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
     if request.method == 'POST':
-        name = random.choice(first_names) + " " + random.choice(last_names)
-    return render_template('index.html', name=name)
+        files = request.files.getlist('file')
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('Files uploaded successfully!')
+        return redirect(url_for('draft'))
+    return render_template('upload.html')
+
+@app.route('/draft')
+def draft():
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    sentences = []
+    for file in files:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file)
+        if file.endswith('.pdf'):
+            with open(filepath, 'rb') as f:
+                reader = PyPDF2.PdfReader(f)
+                page = reader.pages[0]
+                sentences.append(page.extract_text().split('.')[0])
+        elif file.endswith('.docx'):
+            doc = Document(filepath)
+            sentences.append(doc.paragraphs[0].text.split('.')[0])
+        elif file.endswith('.txt'):
+            with open(filepath, 'r') as f:
+                sentences.append(f.readline().split('.')[0])
+        
+        # Delete the file after reading the first sentence
+        os.remove(filepath)
+        
+    return render_template('draft.html', sentences=sentences)
+
+@app.route('/logout')
+def logout():
+    redirect_uri = "https%3A%2F%2Fwww.zispire.com%2F"
+    logout_url = f"https://{TENANT}.b2clogin.com/{TENANT}.onmicrosoft.com/{POLICY}/oauth2/v2.0/logout?post_logout_redirect_uri={redirect_uri}"
+    return redirect(logout_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
